a3bccbebed83c43a1944485e6c5292af77467b2a
sahildongre20 <54145208+sahildongre20@users.noreply.github.com>
2019-10-03 23:21:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/camera/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.camera;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView iv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn =(Button)findViewById(R.id.btn);
+        ImageView iv =(ImageView)findViewById(R.id.iv);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Bitmap bitmap =(Bitmap) data.getExtras().get("data");
+         iv.setImageBitmap(bitmap);
+
+    }
+}
